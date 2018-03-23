d24c3c8ee4f27db9353fc1ed8e6d17088684e398
Vernon Mensah <vernonmensah@gmail.com>
2018-03-23 00:09:58 -0400

    adjustments for aws ses config vars

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test',
+  // AWS SES config
   SECRETACCESSKEY: process.env.SECRETACCESSKEY,
   ACCESSKEYID: process.env.ACCESSKEYID,
+  SENDER: process.env.SENDER,
 };
